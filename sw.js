c216f01052379aa38dae90fbc5373692dd64a7d7
P.Biswanath Patra <63574390+roshanbiswanath@users.noreply.github.com>
2021-07-13 21:28:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('myCache').then(function(cache) {
       return cache.addAll(
-        ['/','index.html'
+        ['/main.css','index.js','index.html'
         ]
       );
     })

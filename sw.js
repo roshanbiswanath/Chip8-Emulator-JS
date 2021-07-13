f74d1c91293fcf80f31de0707f7a1f33ba4aeb35
P.Biswanath Patra <63574390+roshanbiswanath@users.noreply.github.com>
2021-07-13 21:33:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,5 +14,10 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener(fetch, (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
   console.log('Inside the fetch handler:', event);
 });
